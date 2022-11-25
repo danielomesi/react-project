4cac2201152c0d75883675c657d2db933beb7e54
Daniel Omesi <101827280+danielomesi@users.noreply.github.com>
2022-11-25 17:48:16 +0200

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchBar from "./Search Bar/SearchBar.js"
+
+const App = () =>
+{
+    const item="daniel"
+    return (
+        <div>
+           <SearchBar />
+        </div>
+
+    )
+}
+
+
+export default App
